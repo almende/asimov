04ac2ccf37a33839afe34be90be99729ca5f5999
Suki van Beusekom <suki@almende.org>
2015-05-28 12:56:02 +0200

    Bug fix for priority locations in transit

--- src/main/java/io/asimov/model/resource/ActivityParticipantImpl.java
+++ src/main/java/io/asimov/model/resource/ActivityParticipantImpl.java
@@ -99,7 +99,7 @@ public class ActivityParticipantImpl extends
 
 	protected void updateHighestPriorityActivityLocation() {
 		synchronized (this.highestPriorityActivityLocationRepr) {
-			this.highestPriorityActivityLocationRepr.clear();
+			//this.highestPriorityActivityLocationRepr.clear();
 			int maxPriority = Integer.MIN_VALUE; // lowest
 			activities: for (Entry<Request, Integer> entry : this.priorities
 					.entrySet()) {
@@ -119,12 +119,12 @@ public class ActivityParticipantImpl extends
 							continue activities;
 						}
 					}
-				if (!targetFound)
-					LOG.error(
-							"No target resource specified among other resources: "
-									+ JsonUtil.toPrettyJSON(entry.getKey()
-											.getOtherResourceInfo()),
-							new NullPointerException());
+//				if (!targetFound)
+//					LOG.error(
+//							"No target resource specified among other resources: "
+//									+ JsonUtil.toPrettyJSON(entry.getKey()
+//											.getOtherResourceInfo()),
+//							new NullPointerException());
 			}
 			LOG.info("New priority locations: "
 					+ this.highestPriorityActivityLocationRepr);
